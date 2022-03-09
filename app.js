09ffa0424fcdc5e2be68c1603fb50f658c8b4f00
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-03-09 21:55:11 +0800

    [Passport and Service] deleted passport and service folder

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
-const cookieSession = require('cookie-session');
-require('./config/passport-setup');
-require('dotenv').config();
+const cookieSession = require("cookie-session");
+require("dotenv").config();
 
 const app = express();
 
@@ -14,26 +13,26 @@ const forecastRoute = require("./router/forecastRouter");
 const carparkRoute = require("./router/carparkRouter");
 const fillDatabaseRoute = require("./router/fillDatabaseRouter");
 
-
 app.use(cors());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(cookieSession({
-    name:'session', 
-    keys : ['key1', 'key2'],
-    secret: 'anything'
-}))
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+    secret: "anything",
+  })
+);
 
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 
-app.use('/user', userRoute);
-app.use('/review', reviewRoute);
-app.use('/fav', favoriteRoute);
-app.use('/forecast', forecastRoute);
-app.use('/carpark', carparkRoute);
-app.use('/fill', fillDatabaseRoute);
-
+app.use("/user", userRoute);
+app.use("/review", reviewRoute);
+app.use("/fav", favoriteRoute);
+app.use("/forecast", forecastRoute);
+app.use("/carpark", carparkRoute);
+app.use("/fill", fillDatabaseRoute);
 
 module.exports = app;
