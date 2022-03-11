a70df1bc4560e76d36dd35b545420bfbefabb289
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-03-11 20:16:44 +0800

    [Review] fix minor bugs from review function

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -1,74 +1,95 @@
-const pool = require('../db.js');
+const pool = require("../db.js");
 
 const GetReviewByCarpark = async (req, res) => {
-    try{
-        const carpark_id = req.body.carpark_id;
-        
-        const queryReviews = await pool.query("SELECT * FROM Review WHERE carpark_id=($1)", [carpark_id]);
-        
-        const reviews = queryReviews.rows;
-        
-        const responseData = [];
-        for(var i=0; i< reviews.length; i++){
-            const user_id = reviews[i].user_id;
-            const queryUser = await pool.query("SELECT username FROM Account WHERE user_id=($1)", [user_id]);
-            const username = queryUser.rows[0].username;
-            responseData.push({
-                username : username,
-                carpark_id : carpark_id,
-                comment : reviews[i].comment,
-                rating : reviews[i].rating
-            })
-        }
-        return res.status(200).json({
-            message: "success",
-            data: responseData,
-            error: false,
-        });
+  try {
+    const carpark_id = req.query.carpark_id;
 
-    }catch(error){
-        return res.status(400).json({
-            message: error.message,
-            data: [],
-            error: true,
-        });
+    if (!carpark_id) {
+      throw Error("No carpark id found!");
     }
+
+    const queryReviews = await pool.query(
+      "SELECT * FROM Review WHERE carpark_id=($1)",
+      [carpark_id]
+    );
+    const reviews = queryReviews.rows;
+    const responseData = [];
+    for (var i = 0; i < reviews.length; i++) {
+      const user_id = reviews[i].user_id;
+      const queryUser = await pool.query(
+        "SELECT username FROM Account WHERE user_id=($1)",
+        [user_id]
+      );
+      const username = queryUser.rows[0].username;
+      responseData.push({
+        username: username,
+        carpark_id: carpark_id,
+        comment: reviews[i].comment,
+        rating: reviews[i].rating,
+      });
+    }
+
+    return res.status(200).json({
+      message: "success",
+      data: responseData,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+      data: [],
+      error: true,
+    });
+  }
 };
 
-const PostReview = async (req,res)=>{
-    try{
-        const user = req.user;
-        
-        const userId = user.id;
-        const carparkId = req.body.carparkId;
-        const comment = req.body.comment;
-        const rating = req.body.rating;
+const PostReview = async (req, res) => {
+  try {
+    const user = req.user;
+    const userId = user.id;
+    const carparkId = req.body.carparkId;
+    const comment = req.body.comment;
+    const rating = req.body.rating;
 
-        if(!userId || !carparkId || !comment || !rating) throw Error("Missing data");
-
-        const queryReview = await pool.query("INSERT INTO Review(user_id, carpark_id, comment, rating) VALUES($1,$2,$3,$4) RETURNING *",
-                            [userId, carparkId, comment, rating]);
-        
-        if(!queryReview) throw Error("fail to insert to the database");
-
-        return res.status(200).json({
-            message: "success",
-            data: queryReview.rows[0],
-            error: false,
-        });
-
-    }catch(error){
-        return res.status(400).json({
-            message: error.message,
-            data: [],
-            error: true,
-        });
+    if (!userId) {
+      throw Error("Missing user id");
     }
-}
+    if (!carparkId) {
+      throw Error("Missing carpark id");
+    }
+    if (!rating) {
+      throw Error("Missing rating number");
+    }
+
+    const queryReview = await pool.query(
+      "INSERT INTO Review(user_id, carpark_id, comment, rating) VALUES($1,$2,$3,$4) RETURNING *",
+      [userId, carparkId, comment, rating]
+    );
+
+    if (!queryReview) throw Error("fail to insert to the database");
+
+    return res.status(200).json({
+      message: "success",
+      data: {
+        username: user.name,
+        carpark_id: carparkId,
+        comment: comment,
+        rating: rating,
+      },
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+      data: [],
+      error: true,
+    });
+  }
+};
 
 const ReviewController = {
-    GetReviewByCarpark,
-    PostReview
-}
+  GetReviewByCarpark,
+  PostReview,
+};
 
-module.exports = ReviewController
+module.exports = ReviewController;
--- helperFunctions/verifyToken.js
+++ helperFunctions/verifyToken.js
@@ -1,18 +1,19 @@
 //middleware
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-module.exports = (req,res,next)=>{
-    try{
-        const token = req.header('auth-token')
-        if(!token) throw Error ("access denied")
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET)
-        if(!verified) throw Error ("invalid token")
-        req.user = verified
-    }catch(err){
-        res.status(400).json({
-            message: err.message,
-            data: [],
-            error: true})
-    }
-    next() 
-}
+module.exports = (req, res, next) => {
+  try {
+    const token = req.header("auth-token");
+    if (!token) throw Error("access denied");
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    if (!verified) throw Error("invalid token");
+    req.user = verified;
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+      data: [],
+      error: true,
+    });
+  }
+  next();
+};
