3a70a0764585f085af61dbd80a8422aa55aeb815
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-03-15 21:07:41 +0800

    [Favorite] changed response for favorite to carpark details

--- controller/favoriteController.js
+++ controller/favoriteController.js
@@ -1,64 +1,84 @@
 const pool = require("../db.js");
 
-const SetFavorite = async(req,res) =>{
-    try{
-        const user = req.user;
+const SetFavorite = async (req, res) => {
+  try {
+    const user = req.user;
 
-        const userId = user.id;
-        const carparkId = req.body.carpark_id;
+    const userId = user.id;
+    const carparkId = req.body.carpark_id;
 
-        if(!userId || !carparkId) throw Error("Missing data");
+    if (!userId || !carparkId) throw Error("Missing data");
 
-        const checkExistence = await pool.query("SELECT * FROM Favorite WHERE user_id=($1) AND carpark_id=($2)", [userId, carparkId]);
-        if(checkExistence.rows[0]) throw Error("Already set to favorite");
+    const checkExistence = await pool.query(
+      "SELECT * FROM Favorite WHERE user_id=($1) AND carpark_id=($2)",
+      [userId, carparkId]
+    );
+    if (checkExistence.rows[0]) throw Error("Already set to favorite");
 
-        const queryFavorite = await pool.query("INSERT INTO Favorite(user_id, carpark_id) VALUES($1,$2) RETURNING *",
-                                [userId, carparkId]);
-        
-        if(!queryFavorite) throw Error("fail to insert to the database");
+    const insertFavorite = await pool.query(
+      "INSERT INTO Favorite(user_id, carpark_id) VALUES($1,$2) RETURNING *",
+      [userId, carparkId]
+    );
 
-        return res.status(200).json({
-            message: "success",
-            data: queryFavorite.rows[0],
-            error: false,
-        });
+    if (!insertFavorite) throw Error("fail to insert to the database");
 
-    } catch(error){
-        return res.status(400).json({
-            message: error.message,
-            data: [],
-            error: true,
-        });
+    const queryFavorite = await pool.query(
+      "SELECT * FROM Carpark WHERE _id = ($1)",
+      [carparkId]
+    );
+
+    return res.status(200).json({
+      message: "success",
+      data: queryFavorite.rows[0],
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+      data: [],
+      error: true,
+    });
+  }
+};
+
+const GetFavoriteByUser = async (req, res) => {
+  try {
+    const user = req.user;
+    const userId = user.id;
+
+    if (!userId) throw Error("Missing data");
+
+    const queryFavorite = await pool.query(
+      "SELECT * FROM Favorite WHERE user_id = ($1)",
+      [userId]
+    );
+    if (!queryFavorite) throw Error("fail to retrieve from database");
+
+    let carparks = [];
+    for (let i = 0; i < queryFavorite.rows.length; i++) {
+      const queryCarpark = await pool.query(
+        "SELECT * FROM Carpark WHERE _id = ($1)",
+        [queryFavorite.rows[i].carpark_id]
+      );
+      carparks.push(queryCarpark.rows[0]);
     }
-}
 
-const GetFavoriteByUser = async (req,res) =>{
-    try{
-        const user = req.user;
-        const userId = user.id;
-
-        if(!userId) throw Error("Missing data");
-
-        const queryFavorite = await pool.query("SELECT * FROM Favorite WHERE user_id = ($1)", [userId]);
-        if(!queryFavorite) throw Error("fail to retrieve from database");
-
-        return res.status(200).json({
-            message: "success",
-            data: queryFavorite.rows,
-            error: false,
-        });
-
-    } catch(error){
-        return res.status(400).json({
-            message: error.message,
-            data: [],
-            error: true,
-        });
-    }
-}
+    return res.status(200).json({
+      message: "success",
+      data: carparks,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+      data: [],
+      error: true,
+    });
+  }
+};
 const FavoriteController = {
-    SetFavorite,
-    GetFavoriteByUser
-}
+  SetFavorite,
+  GetFavoriteByUser,
+};
 
 module.exports = FavoriteController;
