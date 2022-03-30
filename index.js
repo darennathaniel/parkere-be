f631c0a40e127bdb56deda6653b6372a5f1feb6f
trung <trunghm.88888@gmail.com>
2022-03-30 16:40:50 +0800

    edit port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const http = require("http");
 const server = http.createServer(app);
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 80;
 
 server.listen(port, () => {
   console.log(`Server listening at port ${port}`);
