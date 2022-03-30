3c736ac0ac44969e2ed17b28a4842306ac3c4b66
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-03-30 23:02:06 +0800

    [Limit Payload] increase limit payload

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const fillDatabaseRoute = require("./router/fillDatabaseRouter");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 
 app.use(
   cookieSession({
