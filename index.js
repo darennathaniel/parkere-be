3ad8c8fff76239442411cbbe16956f969f552d9c
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-03-30 19:48:13 +0800

    [Host] added host

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ require("dotenv").config();
 const http = require("http");
 const server = http.createServer(app);
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 4000;
+const host = "110.238.104.188";
 
-server.listen(port, () => {
+server.listen(port, host, () => {
   console.log(`Server listening at port ${port}`);
 });
