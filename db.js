adc835ffdf332e7d1701ffabc5a4ae44a05b0ac8
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2022-03-31 12:53:23 +0800

    [Bug] fixed bugs

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const devConfig = `postgresql://darennathanieljanto:password@localhost:5432/parkere`;
+const devConfig = `postgresql://root:password@localhost:5432/parkere`;
 
 const proConfig = process.env.DATABASE_URL; // for production put database url in .env
 const pool = new Pool({
@@ -11,9 +11,3 @@ const pool = new Pool({
 // For production, set NODE_ENV = production in .env
 
 module.exports = pool;
-
-// PG_USER =darennathanieljanto
-// PG_PASSWORD=password
-// PG_HOST = localhost
-// PG_PORT = 5432
-// PG_DATABASE = parkere
